ee01fdd2627eda04acd35b95b0c9ce282625119c
Edward Breen <ted.breen@gmail.com>
2014-10-22 01:11:24 -0700

    fixed spacing on check_result.js

--- js/check_result.js
+++ js/check_result.js
@@ -1,9 +1,9 @@
 function checkResult(result) {
-	if (result[0] !== result[1]) {
-		return "You lose";
-	}
-	if (result[1] !== result[2]) {
-		return "You lose";
-	}
-	return "You win";
+    if (result[0] !== result[1]) {
+        return "You lose";
+    }
+    if (result[1] !== result[2]) {
+        return "You lose";
+    }
+    return "You win";
 }
